0027b0fd80c916b8eda589c69c63e8ad0e11d7ae
currysensei <currysensei@outlook.com>
2014-10-19 13:33:30 -0400

    ParserText now handles input flags.

--- src/edu/icom4029/cool/tests/ParserTest.java
+++ src/edu/icom4029/cool/tests/ParserTest.java
@@ -4,19 +4,23 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import edu.icom4029.cool.core.Flags;
 import edu.icom4029.cool.lexer.Lexer;
 import edu.icom4029.cool.parser.Parser;
 
 public class ParserTest {
 
 	public static void main(String[] args) {
+		args = Flags.handleFlags(args);
+		
 		for (int i = 0; i < args.length; i++) {
 			FileReader file = null;
+			
 			try {
 				file = new FileReader(args[i]);
-				/* create a parsing object */
 				Parser parser_obj = new Parser(new Lexer(file));
 				parser_obj.parse();
+				
 			} catch (FileNotFoundException ex) {
 				System.out.println("Could not open input file " + args[i]);
 	    	} catch (IOException ex) {
@@ -26,5 +30,4 @@ public class ParserTest {
 			}
 		}
 	}
-
 }
