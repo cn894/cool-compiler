a70b96f9fa84d64f2a3afcd0ffab489f5c7a5678
fabian-zapata <fabianzapata187@gmail.com>
2014-10-31 14:59:42 -0400

    Implemented ClassTable so that it generates a class inheritance graph. Modified bad.cl to include a class inheritance cycle.

--- src/edu/icom4029/cool/parser/ClassTable.java
+++ src/edu/icom4029/cool/parser/ClassTable.java
@@ -24,6 +24,10 @@ PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 // This is a project skeleton file
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import edu.icom4029.cool.lexer.AbstractSymbol;
 import edu.icom4029.cool.lexer.AbstractTable;
@@ -34,6 +38,12 @@ import edu.icom4029.cool.lexer.AbstractTable;
 public class ClassTable {
     private int semantErrors;
     private PrintStream errorStream;
+    
+    private HashMap<String, ArrayList<String>> adjacencyList;
+    public HashMap<String, class_> nameToClass;
+    
+    private HashMap<String, Integer> depthList;
+    private Classes basicClassList;
 
     /** Creates data structures representing basic Cool classes (Object,
      * IO, Int, Bool, String).  Please note: as is this method does not
@@ -145,6 +155,25 @@ public class ClassTable {
 
 		/* Do somethind with Object_class, IO_class, Int_class,
            Bool_class, and Str_class here */
+		
+		nameToClass.put(TreeConstants.Object_.getString(), Object_class);
+		nameToClass.put(TreeConstants.IO.getString(), IO_class);
+		nameToClass.put(TreeConstants.Int.getString(), Int_class);
+		nameToClass.put(TreeConstants.Bool.getString(), Bool_class);
+		nameToClass.put(TreeConstants.Str.getString(), Str_class);
+		
+		adjacencyList.put(TreeConstants.Object_.getString(), new ArrayList<String>());
+		adjacencyList.put(TreeConstants.IO.getString(), new ArrayList<String>());
+		adjacencyList.put(TreeConstants.Int.getString(), new ArrayList<String>());
+		adjacencyList.put(TreeConstants.Bool.getString(), new ArrayList<String>());
+		adjacencyList.put(TreeConstants.Str.getString(), new ArrayList<String>());
+		
+		basicClassList = new Classes(0);
+		basicClassList.appendElement(Object_class);
+		basicClassList.appendElement(IO_class);
+		basicClassList.appendElement(Int_class);
+		basicClassList.appendElement(Bool_class);
+		basicClassList.appendElement(Str_class);
 
 		// NOT TO BE INCLUDED IN SKELETON
 		
@@ -154,16 +183,242 @@ public class ClassTable {
 //		Bool_class.dump_with_types(System.err, 0);
 //		Str_class.dump_with_types(System.err, 0);
     }
-	
-
 
     public ClassTable(Classes cls) {
-		semantErrors = 0;
-		errorStream  = System.err;
+    	semantErrors = 0;
+    	errorStream  = System.err;
 		
 		/* fill this in */
+		// Build graph first out of class declarations
+    	adjacencyList = new HashMap<String, ArrayList<String>>();
+    	nameToClass = new HashMap<String, class_>();
+    	depthList = new HashMap<String, Integer>();
+		
+		installBasicClasses();
+		buildGraph(cls);
+		assignDepths("Object", 0);
     }
 
+    /**
+     * Returns the depth of a node given its name.
+     * @param node The name of the desired node.
+     * @return The depth of the given node.
+     */
+    public int getDepth(String node) {
+    	return depthList.get(node);
+    }
+    
+    /**
+     * Assign depths to every node in the inheritance tree.
+     * @param node The starting node of the tree.
+     * @param depth The depth to be assigned
+     */
+    private void assignDepths(String node, int depth) {
+    	depthList.put(node, depth);
+    	ArrayList<String> children = adjacencyList.get(node);
+    	
+    	if (children == null) return;
+    	if (!children.isEmpty()) {
+    		depth = depth + 1;
+    		for (String child : children) {
+    			assignDepths(child, depth);
+    		}
+    	}
+    }
+    
+    /**
+     * Builds an adjacency list of the inheritance graph
+     * while making sure there are no cycles.
+     * @param classList
+     */
+    private void buildGraph(Classes classList) {
+    	adjacencyList.put(TreeConstants.Object_.getString(), new ArrayList<String>());
+    	
+    	// Add primitive classes as children of Object.
+    	ArrayList<String> objectList = adjacencyList.get(TreeConstants.Object_.getString());
+    	objectList.add(TreeConstants.IO.getString());
+    	objectList.add(TreeConstants.Int.getString());
+    	objectList.add(TreeConstants.Bool.getString());
+    	objectList.add(TreeConstants.Str.getString());
+    	adjacencyList.put(TreeConstants.Object_.getString(), objectList);
+    	
+    	for (Enumeration e = classList.getElements(); e.hasMoreElements(); ) {
+    		class_ currentClass = ((class_) e.nextElement());    		
+    		String className = currentClass.getName().toString();
+    		
+    		// Check that the class has not yet been defined. Else, produce a semant error.
+    		if (!nameToClass.containsKey(className)) {
+    			nameToClass.put(className, currentClass);
+    		}
+    		else {
+    			semantError(currentClass).println("Class " + className + " was previously defined.");
+    			continue;
+    		}
+    		
+    		// If parent already exists, append the child to the list of children.
+    		String parent = currentClass.getParent().toString();
+    		if (!adjacencyList.containsKey(parent)) {
+    			adjacencyList.put(parent, new ArrayList<String>());
+    		}
+    		
+    		adjacencyList.get(parent).add(currentClass.getName().toString());
+    	}
+    	
+    	// Check that each parent in a parent-child inheritance is valid.
+    	HashSet<String> bogusClasses = new HashSet<String>();
+    	for (String parent : adjacencyList.keySet()) {
+    		if (!nameToClass.containsKey(parent)) {
+    			for (String child : adjacencyList.get(parent)) {
+    				semantError(nameToClass.get(child)).println("Class " + child +" inherits from an undefined class "+ parent);
+    			}
+    			// Add the bogus parent class to bogusClasses so it can be removed later.
+    			bogusClasses.add(parent);
+    		}
+    	}
+    	
+    	// Remove all the bogus classes from the inheritance graph.
+    	for (String bogusClass : bogusClasses) {
+    		adjacencyList.remove(bogusClass);
+    	}
+    	
+    	// Check if any class is inheriting from Int, Bool or String
+    	for (String child : adjacencyList.get(TreeConstants.Int.getString())) {
+    		semantError(nameToClass.get(child)).println("Class " + child + " illegally inherits from class Int");
+    	}
+    	for (String child : adjacencyList.get(TreeConstants.Bool.getString())) {
+    		semantError(nameToClass.get(child)).println("Class " + child + " illegally inherits from class Bool");
+    	}
+    	for (String child : adjacencyList.get(TreeConstants.Str.getString())) {
+    		semantError(nameToClass.get(child)).println("Class " + child + " illegally inherits from class String");
+    	}
+    	
+    	// Return before checking for cycles of there are errors.
+    	if (errors()) {
+    		return;
+    	}
+    	
+    	// Now check for cycles, using a depth first search starting from Object
+    	HashMap<String, Boolean> visited = new HashMap<String, Boolean>();
+    	for (String key : adjacencyList.keySet()) {
+    		visited.put(key, false);
+    		for (String value : adjacencyList.get(key)) {
+    			visited.put(value, false);
+    		}
+    	}
+    	
+    	depthFirstSearch(visited, TreeConstants.Object_.getString());
+    	for (String key : visited.keySet()) {
+    		if (!visited.get(key)) {
+    			semantError(nameToClass.get(key)).println("Class " + key + ", or its ancestors, is involved in a cycle");
+    		}
+    	}
+    }
+    
+    private boolean depthFirstSearch(HashMap<String, Boolean> visited, String node) {
+    	if (visited.get(node)) {
+    		// This shouldn't happen, but let's be safe.
+    		semantError(nameToClass.get(node)).println("Class " + node + ", or its ancestors, is involved in a cycle");
+    		return false;
+    	}
+    	visited.put(node, true);
+    	if (adjacencyList.get(node) == null) {
+    		return true;
+    	}
+    	for (String child : adjacencyList.get(node)) {
+    		depthFirstSearch(visited, child);
+    	}
+    	return true;
+    }
+    
+    /**
+     * Checks if the given AbstractSymbol corresponds to a valid type of class.
+     * @param type an AbstractSymbol object corresponding to a type or class
+     * @return true if the symbol is a valid type or class
+     */
+    public boolean isValidType(AbstractSymbol type) {
+    	return nameToClass.containsKey(type.getString());
+    }
+    
+    /**
+     * Checks if C1 <= C2
+     * @param C1
+     * @param C2
+     * @param currentClass
+     * @return
+     */
+    public boolean checkConformance(AbstractSymbol C1, AbstractSymbol C2, AbstractSymbol currentClass) {
+    	if (C1 == TreeConstants.SELF_TYPE) {
+    		C1 = currentClass;
+    	}
+    	if (C2 == TreeConstants.SELF_TYPE) {
+    		C2 = currentClass;
+    	}
+    	if (C1 == C2) {
+    		return true;
+    	}
+    	return isReachable(C1.getString(), C2.getString());
+    }
+    
+    private boolean isReachable(String C1, String C2) {
+    	if (adjacencyList.get(C2) == null) {
+    		return false;
+    	}
+    	for (String child : adjacencyList.get(C2)) {
+    		if (child.equals(C1)) {
+    			return true;
+    		}
+    		else if (isReachable(C1, child)) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    /**
+     * Returns the parent class of a given class.
+     * @param child the child class
+     * @return the name of the parent class
+     */
+    public String getParent(String child) {
+    	if (child.equals(TreeConstants.Object_.getString())) {
+    		return TreeConstants.No_class.getString();
+    	}
+    	for (String parent : adjacencyList.keySet()) {
+    		ArrayList<String> listOfChildren = adjacencyList.get(parent);
+    		if (listOfChildren.indexOf(child) != -1) {
+    			return parent;
+    		}
+    	}
+    	// Shouldn't happen, but it's here to avoid compilation errors anyways.
+    	return null;
+    }
+    
+    /**
+     * Returns a list containing the child classes of a given class. 
+     * @param parent the parent class
+     * @return the list containing all the child classes
+     */
+    public ArrayList<String> getChildren(String parent) {
+    	return adjacencyList.get(parent);
+    }
+    
+    /**
+     * Returns a list of classes containing the Object, IO, Int, Bool and String classes.
+     * @return the basic class list
+     */
+    public Classes getBasicClassList() {
+    	return basicClassList;
+    }
+    
+    /**
+     * Returns a class_ object corresponding to a given name.
+     * @param className the name of the class
+     * @return the corresponding class
+     */
+    public class_ getClass(String className) {
+    	return nameToClass.get(className);
+    }
+    
     /** Prints line number and file name of the given class.
      *
      * Also increments semantic error count.
@@ -188,7 +443,7 @@ public class ClassTable {
      *
      * */
     public PrintStream semantError(AbstractSymbol filename, TreeNode t) {
-		errorStream.print(filename + ":" + t.getLineNumber() + ": ");
+    	this.errorStream.print(filename + ":" + t.getLineNumber() + ": ");
 		return semantError();
     }
 
@@ -200,13 +455,13 @@ public class ClassTable {
      *
      * */
     public PrintStream semantError() {
-    	semantErrors++;
-    	return errorStream;
+    	this.semantErrors++;
+    	return this.errorStream;
     }
 
     /** Returns true if there are any static semantic errors. */
     public boolean errors() {
-    	return semantErrors != 0;
+    	return this.semantErrors != 0;
     }
 
     // NOT TO BE INCLUDED IN SKELETON
