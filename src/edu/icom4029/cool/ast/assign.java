eb866d8251f938ec7b19aa6252e9f573d7a08447
currysensei <currysensei@outlook.com>
2014-11-10 15:06:45 -0400

    meep

--- src/edu/icom4029/cool/ast/assign.java
+++ src/edu/icom4029/cool/ast/assign.java
@@ -67,7 +67,6 @@ public class assign extends Expression {
 		}
 		
 		AbstractSymbol nameType = (AbstractSymbol) symbolTable.lookup(name);
-		System.out.println(nameType);
 		expr.semant(classTable, cl, symbolTable); // Call the semant method of the RHS expression of the assign expression.
 
 		AbstractSymbol exprType = expr.get_type();
