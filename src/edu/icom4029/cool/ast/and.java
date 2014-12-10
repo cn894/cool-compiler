0ee61a1cfdbad490a24c7813e03e48f648edb698
Fabian Zapata <fabianzapata187@gmail.com>
2014-12-10 09:23:41 -0400

    Added skeleton "and" and "or" files in ast.

---
+++ src/edu/icom4029/cool/ast/and.java
@@ -0,0 +1,49 @@
+package edu.icom4029.cool.ast;
+
+import java.io.PrintStream;
+
+import edu.icom4029.cool.ast.base.TreeNode;
+import edu.icom4029.cool.semant.ClassTable;
+import edu.icom4029.cool.semant.SymbolTable;
+
+public class and extends Expression {
+	protected Expression e1;
+	protected Expression e2;
+	
+	public and(int lineNumber, Expression a1, Expression a2) {
+		super(lineNumber);
+		e1 = a1;
+		e2 = a2;
+	}
+
+	@Override
+	public void dump_with_types(PrintStream out, int n) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void semant(ClassTable classTable, class_ cl, SymbolTable symbolTable) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void code(PrintStream s) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public TreeNode copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void dump(PrintStream out, int n) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
